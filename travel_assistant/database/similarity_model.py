571365ff933d81acd6339bc18cf6a9bdebb15877
Lotusbro98 <lotusbro98@gmail.com>
2024-03-31 11:58:37 +0300

    new database

--- travel_assistant/database/similarity_model.py
+++ travel_assistant/database/similarity_model.py
@@ -1,13 +1,15 @@
 from typing import List
 
 import numpy as np
+import torch
 from sentence_transformers import SentenceTransformer
 
 
 class SimilarityModel:
     def __init__(self):
-        self.model = SentenceTransformer('sentence-transformers/distiluse-base-multilingual-cased-v2')
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.model = SentenceTransformer('sentence-transformers/distiluse-base-multilingual-cased-v2', device=self.device)
 
     def encode(self, sentences: str | List[str]) -> np.ndarray:
-        embeddings = self.model.encode(sentences)
+        embeddings = self.model.encode(sentences, device=self.device)
         return embeddings
