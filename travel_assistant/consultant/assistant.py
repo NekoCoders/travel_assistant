efd25b03b536a59efdf1b5b86c544c704bcaf4b1
Alexander Panyukov <adpanyukov@sberbank.ru>
2024-04-06 20:57:55 +0300

    refactored assistant code

--- travel_assistant/consultant/assistant.py
+++ travel_assistant/consultant/assistant.py
@@ -1,25 +1,13 @@
-# from langchain.schema import HumanMessage, SystemMessage
 from __future__ import annotations
 
 import json
-import re
-from typing import List, Tuple, Union, Callable, Any
+from typing import List, Tuple
 
-from langchain.agents.format_scratchpad import format_log_to_str
-from langchain.agents.output_parsers import XMLAgentOutputParser, JSONAgentOutputParser
-from langchain.tools.render import ToolsRenderer, render_text_description_and_args
-from langchain_core.agents import AgentAction, AgentFinish
-from langchain_core.exceptions import OutputParserException
-from langchain_core.messages import AIMessage, HumanMessage
 from langchain_core.output_parsers import StrOutputParser
 from langchain.chat_models.gigachat import GigaChat
-from langchain_core.output_parsers.json import parse_partial_json, _custom_parser
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain import hub
-from langchain.agents import AgentExecutor, tool, create_structured_chat_agent, AgentOutputParser
-from langchain_core.runnables import RunnablePassthrough
+from langchain.agents import AgentExecutor, tool
 
-from travel_assistant.common.custom_types import Product
 from travel_assistant.common.gigachat_api import AUTH_DATA
 from travel_assistant.consultant.agent_utils import create_agent
 from travel_assistant.consultant.assistant_prompts import system, human, system_ask, human_ask
@@ -27,64 +15,67 @@ from travel_assistant.database.database import ProductDatabase
 
 
 class Assistant:
-    def __init__(self):
+    def __init__(self, verbose=False):
+        self.verbose = verbose
         self.llm = GigaChat(model="GigaChat", credentials=AUTH_DATA, verify_ssl_certs=False)
         self.database = ProductDatabase()
         self.database.load()
         self.database.save()
 
-    def chat_single(self, context: List[Tuple[str, str]], user_message: str):
-        prompt = ChatPromptTemplate.from_messages(
-            [
-                ("system", system),
-                MessagesPlaceholder("chat_history", optional=True),
-                ("human", human),
-            ]
-        )
+        self.tools = []
+        self.register_search_places_tool()
 
-        llm = self.llm.bind(function_call="none")
+        self.create_agent()
 
-        @tool
-        def search_places(query: str) -> str:
-            """
-            Данный инструмент позволяет найти в базе RUSSPASS места для прогулки или путешествия по их описанию.
-            Используй этот инструмент всегда, когда готов порекомендовать пользователю места.
+    def register_search_places_tool(self):
+      @tool
+      def search_places(query: str) -> str:
+        """
+        Данный инструмент позволяет найти в базе RUSSPASS места для прогулки или путешествия по их описанию.
+        Используй этот инструмент всегда, когда готов порекомендовать пользователю места.
 
-            :param query: Описание места, которое вы бы хотели посетить
-            :return: Предложения, которые есть в базе
-            """
-            products = self.database.search_best_offers(query)
-            output_info = f"\nПредложения, которые я нашел в базе RUSSPASS по запросу '{query}':\n"
-            output_info += "\n".join([f" - {p.title}: {p.description}" for p in products])
-            output_info += "\n"
+        :param query: Описание места, которое вы бы хотели посетить
+        :return: Предложения, которые есть в базе
+        """
+        products = self.database.search_best_offers(query)
+        output_info = f"\nПредложения, которые я нашел в базе RUSSPASS по запросу '{query}':\n"
+        output_info += "\n".join([f" - {p.title}: {p.description}" for p in products])
+        output_info += "\n"
 
-            return output_info
+        return output_info
+      self.tools.append(search_places)
 
-        tool_list = [search_places]
+    def create_agent(self):
+        prompt = ChatPromptTemplate.from_messages([
+            ("system", system),
+            MessagesPlaceholder("chat_history", optional=True),
+            ("human", human),
+        ])
 
-        agent = create_agent(prompt, llm, tool_list)
+        agent = create_agent(prompt, self.llm, self.tools)
+        self.agent_executor = AgentExecutor(agent=agent, tools=self.tools, verbose=self.verbose)
 
-        agent_executor = AgentExecutor(agent=agent, tools=tool_list, verbose=True)
-
-        agent_output = agent_executor.invoke(
+    def chat_single(self, context: List[Tuple[str, str]], user_message: str) -> Tuple[List[Tuple[str, str]], str, List[str]]:
+        agent_output = self.agent_executor.invoke(
             {
                 "input": user_message,
                 "chat_history": context,
             }
         )
-        bot_response = agent_output["output"]["text"]
+        bot_response = agent_output["output"]
+        if isinstance(bot_response, dict):
+            bot_response = bot_response.values().__iter__().__next__()
 
         question, options = self.ask_questions(context, bot_response)
 
         full_response = f"{bot_response}\n{question}"
-        print(options)
 
         context += [
             ("human", user_message),
             ("ai", full_response)
         ]
 
-        return full_response, context
+        return context, full_response, options
 
     def chat(self):
         messages = [
@@ -102,8 +93,9 @@ class Assistant:
         #     message = input()
         for message in messages:
             print("User:", message)
-            output_message, context = self.chat_single(context, message)
+            context, output_message, options = self.chat_single(context, message)
             print("Bot :", output_message)
+            print(options)
 
     def ask_questions(self, context, bot_response):
         prompt = ChatPromptTemplate.from_messages(
@@ -126,7 +118,7 @@ class Assistant:
 
 
 def main():
-    bot = Assistant()
+    bot = Assistant(verbose=True)
 
     bot.chat()
 
