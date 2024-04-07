ad526184de33972399b6bcc2c32d1e7b72926cd3
Dreyk111 <andr.isachenko@gmail.com>
2024-04-07 08:02:00 +0300

    add empty Django and React apps

---
+++ travel_assistant/web_app/django_project/django_app/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def main_page(request):
+    return render(request, 'index.html')
+
+
+def form_send_message(request):
+    # if this is a POST request we need to process the form data
+    if request.method == "POST":
+        # create a form instance and populate it with data from the request:
+        form = FeedbackForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            message = (f"Заявка на сайте от {form.cleaned_data['name']} {form.cleaned_data['surname']}, "
+                       f"свяжитесь по телефону {form.cleaned_data['tel']}")
+            send_feedback_notification(message=message)
+            # тут отобразить начальную страницу с доп параметром - покажи всплывающее окно!
+            return HttpResponseRedirect("/")
+    else:
+        return HttpResponseRedirect("/")
+
+    return render(request, "name.html")
--- utils/parse_russpass.py
+++ utils/parse_russpass.py
@@ -13,9 +13,8 @@ a = tqdm()
 a.__enter__()
 
 
-def get_items(total_pages: int, page: int = 101) -> list[dict]:
+def get_items(total_pages: int, page: int = 1) -> list[dict]:
     req = {"filters": [], "page": page}
-    a.update()
     headers = {"Rqid": "09b86041-4c70-4458-a377-d8ff89e4b172", "Content-Language": "ru"}
     res = requests.post(url=f"{RUSSPASS_HOST}/search/portal/catalog/placesAndEvents", json=req, headers=headers)
     res_data = res.json()
@@ -27,6 +26,26 @@ def get_items(total_pages: int, page: int = 101) -> list[dict]:
             print("got Exception", str(e))
             return res_objects
         time.sleep(0.6)
+        a.update()
+        return res_objects + next_objects
+    return res_objects
+
+
+def get_excursions(total_pages: int, page: int = 1) -> list[dict]:
+    req = {"page":1,"sorting":"RECOMMENDED","filters":[{"elementType":"TAGS","name":"kind","filterRules":"OR","options":["5ec3ee40bfdeb400197cbe8e"]}]}
+
+    headers = {"Rqid": "8bf1cd2d-f948-43b8-9564-e6c2bce0a0bb", "Content-Language": "ru"}
+    res = requests.get(url=f"{RUSSPASS_HOST}/campaign/excursions?includeCampaign=false&language=ru&page={page}&pageSize=5&parserSource=ALL&query%5Bcategory%5D=2&query%5Bfilters%5D%5Bkind%5D[]=ekskursii", headers=headers)
+    res_data = res.json()
+    res_objects = res_data["items"] if "items" in res_data else []
+    if res_objects and page < total_pages:
+        try:
+            next_objects = get_excursions(total_pages, page=page+1)
+        except Exception as e:
+            print("got Exception", str(e))
+            return res_objects
+        a.update()
+        time.sleep(0.6)
         return res_objects + next_objects
     return res_objects
 
@@ -36,6 +55,29 @@ def get_item_info(item_id: str) -> dict:
     return res.json()
 
 
+def get_excursion_info(item_id: str) -> dict:
+    headers = {"Rqid": "8bf1cd2d-f948-43b8-9564-e6c2bce0a0bb", "Content-Language": "ru"}
+    res = requests.get(url=f"{RUSSPASS_HOST}/campaign/excursion?id={item_id}&language=ru", headers=headers)
+    return res.json()
+
+
+def _parse_item_info(item_info, item_id) -> dict:
+    if "item" not in item_info:
+        if 'code' in item_info:
+            print("Error", item_info['code'])
+        else:
+            print("WTF", item_info)
+        return None
+    title = item_info["item"]["title"]
+    description = item_info["item"]["description"]
+    cities = [c["title"] for c in item_info.get("cities", [])]
+    regions = [c["title"] for c in item_info.get("regions", [])]
+    tags = [c["title"] for c in item_info.get("tags", [])]
+    full_text = f"{title}. Город: {', '.join(cities)}, регион: {', '.join(regions)}. Теги: {', '.join(tags)}. {description}"
+    return {"id": item_id, "title": title, "description": description, "cities": cities, "regions": regions,
+                          "tags": tags, "full_text": full_text}
+
+
 def get_items_descriptions(total_pages: int) -> list[dict]:
     a.total = total_pages
     items = get_items(total_pages=total_pages)
@@ -43,19 +85,19 @@ def get_items_descriptions(total_pages: int) -> list[dict]:
     result_descriptions = []
     for item_id in tqdm(item_ids):
         item_info = get_item_info(item_id=item_id)
-        if "item" not in item_info:
-            if 'code' in item_info:
-                print("Error", item_info['code'])
-            else:
-                print("WTF", item_info)
-            continue
-        title = item_info["item"]["title"]
-        description = item_info["item"]["description"]
-        cities = [c["title"] for c in item_info.get("cities", [])]
-        regions = [c["title"] for c in item_info.get("regions", [])]
-        tags = [c["title"] for c in item_info.get("tags", [])]
-        full_text = f"{title}. Город: {', '.join(cities)}, регион: {', '.join(regions)}. Теги: {', '.join(tags)}. {description}"
-        result_description = {"id": item_id, "title": title, "description": description,"cities": cities, "regions": regions, "tags": tags, "full_text": full_text}
+        result_description = _parse_item_info(item_id=item_id, item_info=item_info)
+        result_descriptions.append(result_description)
+    return result_descriptions
+
+
+def get_excursion_descriptions(total_pages: int) -> list[dict]:
+    a.total = total_pages
+    items = get_excursions(total_pages=total_pages)
+    item_ids = [i["id"] for i in items]
+    result_descriptions = []
+    for item_id in tqdm(item_ids):
+        item_info = get_excursion_info(item_id=item_id)
+        result_description = _parse_item_info(item_id=item_id, item_info=item_info)
         result_descriptions.append(result_description)
     return result_descriptions
 
@@ -65,7 +107,7 @@ def print_random_descriptions():
 
 
 if __name__ == "__main__":
-    TOTAL_PAGES = 1000
-    results = get_items_descriptions(total_pages=TOTAL_PAGES)
-    with open("result_descriptions4.json", "w", encoding="utf-8") as f:
+    TOTAL_PAGES = 110
+    results = get_excursion_descriptions(total_pages=TOTAL_PAGES)
+    with open("result_description_excursions.json", "w", encoding="utf-8") as f:
         json.dump(results, f, indent=2, ensure_ascii=False)
